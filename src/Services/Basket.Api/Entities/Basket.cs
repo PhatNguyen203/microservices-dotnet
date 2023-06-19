0be22878cb5a698ce10f223a202da29a58ef8178
Phat Nguyen <phat.nguyen@preferredcomputers.biz>
2023-06-19 10:59:46 -0500

    added basket business logic and controller

---
+++ src/Services/Basket.Api/Entities/Basket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Entities
+{
+    public class Basket
+    {
+        public string Username { get; set; }
+        public IEnumerable<ShoppingItem> Items { get; set; } = new List<ShoppingItem>();
+        public Basket()
+        {
+
+        }
+        public Basket(string username)
+        {
+            Username = username;
+        }
+
+        public decimal Total
+        {
+            get
+            {
+                decimal total = 0;
+                foreach(var item in Items)
+                {
+                    total += item.Price * item.Quantity;
+                }
+                return total;
+            }
+        }
+    }
+}
---
+++ src/Services/Basket.Api/Repositories/Contracts/IBasketRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Repositories.Contracts
+{
+    public interface IBasketRepository
+    {
+        Task<Entities.Basket> GetBasket(string username);
+        Task<Entities.Basket> UpdateBasket(Entities.Basket basket);
+        Task DeleteBasket(string username);
+    }
+}
