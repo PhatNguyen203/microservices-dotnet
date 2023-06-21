2879c76d93ca727b0531211186665ea37fbd29c2
Phat Nguyen <phat.nguyen@preferredcomputers.biz>
2023-06-21 12:23:15 -0500

    added mapping and bus address

---
+++ src/Services/Basket.Api/Profiles/BasketMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Basket.Api.Entities;
+using EventBus.Messages.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Profiles
+{
+    public class BasketMapping : Profile
+    {
+        public BasketMapping()
+        {
+            CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
